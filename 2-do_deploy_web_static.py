0b0cc9dfc0ce61008e8f552aa6c15cccf371b7dc
Fatima20027 <fatima.hailou@icloud.com>
2024-07-14 21:17:59 +0100

    updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,19 +9,19 @@ from os.path import exists
 env.hosts = ['52.91.153.24', '100.25.150.128']
 
 
+
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
-    
-    file_nm = archive_path.split("/")[-1]
-    no_ext = file_nm.split(".")[0]
-    path = "/data/web_static/releases/"
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_nm, path, no_ext))
-        run('rm /tmp/{}'.format(file_nm))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
