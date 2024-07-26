ea692d5ac5abe1509f4721502e5d26eac4d89c4c
Fatima20027 <fatima.hailou@icloud.com>
2024-07-26 15:24:50 +0100

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+The application listens on all available IP addresses (0.0.0.0) and port 5000.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    Route handler for the root URL.
+
+    Returns:
+        str: The string "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
