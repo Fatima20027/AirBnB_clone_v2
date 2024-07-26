cb2c8471b8ab163633d00a8f5047920252fc7a8b
Fatima20027 <fatima.hailou@icloud.com>
2024-07-26 16:15:00 +0100

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+The application listens on all available IP addresses (0.0.0.0) and port 5000.
+Routes: - /: returns "Hello HBNB!"
+         - /hbnb: returns "HBNB"
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    Returns:
+        str: The string "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returns:
+        str: The string "HBNB!".
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    display “C ” followed by the value of the text variable.
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
